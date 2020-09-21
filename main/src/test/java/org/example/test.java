9f8a5be9937df565b7903acf00e297e19affba6f
ZhengyiWuu <70997287+ZhengyiWuu@users.noreply.github.com>
2020-09-21 22:29:13 +0800

    初步完成代码的编写

---
+++ main/src/test/java/org/example/test.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @org.junit.Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
